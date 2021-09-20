c049135a2b7be467c4ab17ae06557c14eabc6860
Dymion <damianskorupa4@gmail.com>
2021-09-20 21:32:18 +0200

    Add show details feature

--- client/src/app/cars-list/cars-list.component.ts
+++ client/src/app/cars-list/cars-list.component.ts
@@ -3,11 +3,20 @@ import { Car } from '../shared/models/car.model';
 import { CARS } from '../shared/mocks/mock-cars';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-cars-list',
   templateUrl: './cars-list.component.html',
-  styleUrls: ['./cars-list.component.css']
+  styleUrls: ['./cars-list.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
+
 })
 export class CarsListComponent implements AfterViewInit {
   @Input('ELEMENT_DATA')  ELEMENT_DATA!:  Car[];
